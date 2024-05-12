2789de4271f070364d71591691e15595b223285d
Abdulkadir Sarikoc <abdulkadirsarikoc@hotmail.com>
2024-05-12 16:11:50 +0300

    feat: add datetimepicker and fix select gender

--- lib/view/login/complete_profile_view.dart
+++ lib/view/login/complete_profile_view.dart
@@ -14,6 +14,11 @@ class CompleteProfileView extends StatefulWidget {
 
 class _CompleteProfileViewState extends State<CompleteProfileView> {
   TextEditingController txtDate = TextEditingController();
+  TextEditingController txtWeight = TextEditingController();
+  TextEditingController txtHeight = TextEditingController();
+
+  String? selectedGender;
+  DateTime? selectedDate;
 
   @override
   Widget build(BuildContext context) {
@@ -52,141 +57,124 @@ class _CompleteProfileViewState extends State<CompleteProfileView> {
                   padding: const EdgeInsets.symmetric(horizontal: 15.0),
                   child: Column(
                     children: [
+                      // Gender Selection Dropdown
                       Container(
-                          decoration: BoxDecoration(
-                              color: TColor.lightGray,
-                              borderRadius: BorderRadius.circular(15)),
-                          child: Row(
-                            children: [
-                              Container(
-                                  alignment: Alignment.center,
-                                  width: 50,
-                                  height: 50,
-                                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                                  
-                                  child: Image.asset(
-                                    "assets/img/gender.png",
-                                    width: 20,
-                                    height: 20,
-                                    fit: BoxFit.contain,
-                                    color: TColor.gray,
-                                  )),
-                            
-                              Expanded(
-                                child: DropdownButtonHideUnderline(
-                                  child: DropdownButton(
-                                    items: ["Male", "Female"]
-                                        .map((name) => DropdownMenuItem(
-                                              value: name,
-                                              child: Text(
-                                                name,
-                                                style: TextStyle(
-                                                    color: TColor.gray,
-                                                    fontSize: 14),
-                                              ),
-                                            ))
-                                        .toList(),
-                                    onChanged: (value) {},
-                                    isExpanded: true,
-                                    hint: Text(
-                                      "Choose Gender",
-                                      style: TextStyle(
-                                          color: TColor.gray, fontSize: 12),
-                                    ),
-                                  ),
+                        decoration: BoxDecoration(
+                          color: TColor.lightGray,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              alignment: Alignment.center,
+                              width: 50,
+                              height: 50,
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 15),
+                              child: Image.asset(
+                                "assets/img/gender.png",
+                                width: 20,
+                                height: 20,
+                                fit: BoxFit.contain,
+                                color: TColor.gray,
+                              ),
+                            ),
+                            Expanded(
+                              child: DropdownButtonHideUnderline(
+                                child: DropdownButton(
+                                  value: selectedGender,
+                                  items: ["Male", "Female"]
+                                      .map((name) => DropdownMenuItem(
+                                            value: name,
+                                            child: Text(
+                                              name,
+                                              style: TextStyle(
+                                                  color: TColor.gray,
+                                                  fontSize: 14),
+                                            ),
+                                          ))
+                                      .toList(),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      selectedGender = value as String;
+                                    });
+                                  },
+                                  isExpanded: true,
                                 ),
                               ),
-
-                             const SizedBox(width: 8,)
-
-                            ],
-                          ),),
-                      SizedBox(
-                        height: media.width * 0.04,
+                            ),
+                            SizedBox(width: 8),
+                          ],
+                        ),
                       ),
+
+                      SizedBox(height: media.width * 0.04),
+                      GestureDetector(
+                        onTap: () async {
+                          final DateTime? picked = await showDatePicker(
+                            context: context,
+                            initialDate: null,
+                            firstDate: DateTime(1900),
+                            lastDate: DateTime.now(),
+                          );
+                          if (picked != null && picked != selectedDate) {
+                            setState(() {
+                              selectedDate = picked;
+                              txtDate.text =
+                                  "${picked.day}/${picked.month}/${picked.year}";
+                            });
+                          }
+                        },
+                        child: Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: TColor.lightGray,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: TextFormField(
+                            controller: txtDate,
+                            decoration: InputDecoration(
+                              hintText: "Date of Birth",
+                              prefixIcon: Image.asset(
+                                "assets/img/date.png",
+                                width: 20,
+                                height: 20,
+                                color: TColor.gray,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              enabled: false,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: media.width * 0.04),
                       RoundTextField(
-                        controller: txtDate,
-                        hitText: "Date of Birth",
-                        icon: "assets/img/date.png",
+                        controller: txtWeight,
+                        hitText: "Your Weight",
+                        icon: "assets/img/weight.png",
+                        keyboardType: TextInputType.number,
                       ),
-                      SizedBox(
-                        height: media.width * 0.04,
+                      SizedBox(height: media.width * 0.04),
+                      RoundTextField(
+                        controller: txtHeight,
+                        hitText: "Your Height",
+                        icon: "assets/img/hight.png",
+                        keyboardType: TextInputType.number,
                       ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: RoundTextField(
-                              controller: txtDate,
-                              hitText: "Your Weight",
-                              icon: "assets/img/weight.png",
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 8,
-                          ),
-                          Container(
-                            width: 50,
-                            height: 50,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: TColor.secondaryG,
-                              ),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: Text(
-                              "KG",
-                              style:
-                                  TextStyle(color: TColor.white, fontSize: 12),
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: media.width * 0.04,
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: RoundTextField(
-                              controller: txtDate,
-                              hitText: "Your Height",
-                              icon: "assets/img/hight.png",
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 8,
-                          ),
-                          Container(
-                            width: 50,
-                            height: 50,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: TColor.secondaryG,
-                              ),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: Text(
-                              "CM",
-                              style:
-                                  TextStyle(color: TColor.white, fontSize: 12),
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: media.width * 0.07,
-                      ),
+                      SizedBox(height: media.width * 0.07),
                       RoundButton(
-                          title: "Next >",
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const WhatYourGoalView()));
-                          }),
+                        title: "Next >",
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const WhatYourGoalView(),
+                            ),
+                          );
+                        },
+                      ),
                     ],
                   ),
                 ),
